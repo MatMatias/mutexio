f77ac413c9233e9bd43642ac5000aa084dd1a3f4
SamuraX <matheusomr1@gmail.com>
2022-08-01 11:48:52 -0300

    message protocol changed

--- receive_message.py
+++ receive_message.py
@@ -2,22 +2,28 @@ import globals
 
 
 def receive_message(socket):
-    message_header, header_address = socket.recvfrom(globals.MESSAGE_HEADER)
+    # message_header, header_address = socket.recvfrom(globals.MESSAGE_HEADER)
 
-    message_header = message_header.decode(globals.MESSAGE_FORMAT)
+    # message_header = message_header.decode(globals.MESSAGE_FORMAT)
 
-    if message_header:
-        message_length = int(message_header)
+    # if message_header:
+    #     message_length = int(message_header)
 
-        message_data, message_address = socket.recvfrom(message_length)
-        message_data = message_data.decode(globals.MESSAGE_FORMAT)
+    #     message_data, message_address = socket.recvfrom(message_length)
+    #     message_data = message_data.decode(globals.MESSAGE_FORMAT)
 
-        global error_message
-        error_message = (
-            f"[BAD MESSAGE] Bad message from H:{header_address} M:{message_address}"
-        )
+    #     global error_message
+    #     error_message = (
+    #         f"[BAD MESSAGE] Bad message from H:{header_address} M:{message_address}"
+    #     )
 
-        if message_data and message_address == header_address:
-            return (message_data, message_address)
+    #     if message_data and message_address == header_address:
+    #         return (message_data, message_address)
 
-    return error_message
+    message_data, message_address = socket.recvfrom(globals.MESSAGE_HEADER)
+
+    untreated_message = message_data.decode(globals.MESSAGE_FORMAT)
+    last_separator_position = untreated_message.rfind("|")
+    treated_message = untreated_message[:last_separator_position]
+
+    return (treated_message, message_address)
--- send_message.py
+++ send_message.py
@@ -2,11 +2,13 @@ import globals
 
 
 def send_message(socket, message, address):
+    message_ascii_length = len(message.encode(globals.MESSAGE_FORMAT))
+    filler_string = "|"
+    for _ in range(message_ascii_length - 1):
+        filler_string += "0"
+
+    message = message + filler_string
+    message.encode()
     message = message.encode(globals.MESSAGE_FORMAT)
 
-    message_length = len(message)
-    send_length = str(message_length).encode(globals.MESSAGE_FORMAT)
-    send_length += b" " * ((globals.MESSAGE_HEADER) - len(send_length))
-
-    socket.sendto(send_length, address)
     socket.sendto(message, address)

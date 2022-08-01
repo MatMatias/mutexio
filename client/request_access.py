bcba5a3b5939680ce71bc6badf2a245bcaf1263e
SamuraX <matheusomr1@gmail.com>
2022-08-01 12:44:59 -0300

    processes will no longer sleep between requests

--- client/request_access.py
+++ client/request_access.py
@@ -5,8 +5,6 @@ sys.path.append("..")
 import globals
 from send_message import send_message
 
-from time import sleep
-
 
 def request_access(client_socket, client_id, access_granted_event, request_limit):
     request_counter = 0
@@ -17,7 +15,6 @@ def request_access(client_socket, client_id, access_granted_event, request_limit
             message = f"{globals.REQUEST_COMMAND}|{client_id}"
             send_message(client_socket, message, globals.UDP_SERVER_ADDRESS)
             print(f"[MESSAGE SENT] {message}")
-            sleep(2)
 
             request_counter += 1
             if request_counter >= request_limit:
